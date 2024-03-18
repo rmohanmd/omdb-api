35e2b31d92e7208184ab5e9225724e77ae2a9028
Rahul Mohan <88761343+rmohanmd@users.noreply.github.com>
2024-03-18 18:13:32 -0400

    Add API Key
    Added instructions on how to obtain and add users personal api key to project.
    Added gitignore file to ignore apikey.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
-
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+function getFetch() {
+  //obtain your own personal api key from omdbapi.com
+  //create a javascript file in the js folder named apikey.js
+  //in the apikey.js file create a const OMDB_API_Key=""
+  //place your key between the quotes.
+  fetch(`http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=muppets`)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
